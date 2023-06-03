7a9b7fb09a288e0077b4a09f31512a65f0689cb7
hongjiaherng <jiaherng2001@gmail.com>
2023-06-03 17:19:02 +0800

    Finish server for inference endpoints

--- README.md
+++ README.md
@@ -7,11 +7,17 @@ A web application that allows users to get a quote for their medical insurance p
 ## Getting Started
 
 ### Prerequisites
-
-- Python 3.8 or above
-- [Poetry](https://python-poetry.org/) - Python dependency management tool
+- Client-side
+  - `to be filled`
+- Server-side
+  - Python 3.8 or above
+  - [Poetry](https://python-poetry.org/) - Python dependency management tool
 
 ## Usage
+
+### Running the client
+
+`to be filled`
 
 ### Running the web server
 
@@ -19,19 +25,38 @@ A web application that allows users to get a quote for their medical insurance p
 ```sh
 mediquote> cd "./mediquote/server/"
 ```
-2. Activate the virtual environment
+2. Install dependencies ([Guide](https://python-poetry.org/docs/basic-usage/#installing-dependencies))
+```sh
+mediquote/mediquote/server> poetry install
+```
+3. Activate the virtual environment
 ```sh
 mediquote/mediquote/server> poetry shell
 ```
-3. Start the web server
-```sh
-mediquote/mediquote/server> flask --app app --port=5000 run
-``` 
-or 
+4. Start the web server
 ```sh
 mediquote/mediquote/server> python app.py
 ```
+5. Test the web server
+```sh
+mediquote/mediquote/server> python test_app.py
+```
+6. Note
+  - 3 endpoints are available:
+    - `/index` - Home page
+    - `/predict/decision-tree` - predict with decision tree model
+    - `/predict/random-forest` - predict with random forest model
+    - `/predict/mlp` - predict with MLP model
+
+
+## Tools & Technologies
+- Client-side
+  - `to be filled`
+- Server-side
+  - Python (>=3.8), Poetry, Flask, Scikit-learn, Pandas, Numpy, PyTorch
 
 
 ## Acknowledgement
 This project is developed to fulfill a group assignment for the WIE3005 Knowledge Management and Engineering course at [University of Malaya](https://www.um.edu.my/)
+
+Special thanks to the author of [this dataset](https://www.kaggle.com/datasets/tejashvi14/medical-insurance-premium-prediction). Although this dataset might not be robust enough for real-world application, it is sufficient for the purpose of this project to illustrate the idea of predicting premium cost using Machine Learning.
--- mediquote/server/app.py
+++ mediquote/server/app.py
@@ -1,13 +1,86 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
+import json
+import torch
+import numpy as np
+
 
 app = Flask(__name__)
 
+scaler = joblib.load("./models/standard_scaler.joblib")
+models = {
+  "decision-tree": joblib.load("./models/decision_tree.joblib"),
+  "random-forest": joblib.load("./models/random_forest.joblib"),
+  "mlp": torch.load("./models/mlp.pt")
+}
+
+# Set the model to evaluation mode
+models["mlp"].eval()  
+
+# Define the expected fields (order matters)
+expected_fields = [
+  "Age",
+  "Diabetes",
+  "BloodPressureProblems",
+  "AnyTransplants",
+  "AnyChronicDiseases",
+  "Height",
+  "Weight",
+  "KnownAllergies",
+  "HistoryOfCancerInFamily",
+  "NumberOfMajorSurgeries",
+]
 
 @app.route("/")
 def index():
   return "Hello from MediQuote!"
 
+@app.route("/predict/<model_name>", methods=["POST"])
+def predict(model_name):
+  if not request.is_json:
+    return jsonify({"error": "Invalid request: Must pass in JSON"}), 400
+  
+  try:
+    # Get the JSON payload from the request
+    data = json.loads(request.get_json())
+
+    # Check for missing or extra fields
+    missing_fields = set(expected_fields) - set(data.keys())
+    extra_fields = set(data.keys()) - set(expected_fields)
+    if missing_fields:
+      raise KeyError(f"Missing field(s), {missing_fields}")
+    if extra_fields:
+      raise KeyError(f"Found invalid field(s), {extra_fields}")
+    
+    # Extract the features from the JSON payload
+    features = [data[field] for field in expected_fields]
+
+    # Convert the features to a numpy array
+    features = np.array(features, dtype=np.float32).reshape(1, -1)
+
+    # Scale the features
+    features = scaler.transform(features)
+
+    # Make the prediction
+    model = models.get(model_name, None)
+    if model is None:
+      return jsonify({"error": f"Invalid model name: {model_name}"}), 400
+    
+    if isinstance(model, torch.nn.Module):
+      with torch.no_grad():
+        features = torch.tensor(features, dtype=torch.float32).view(1, -1)
+        prediction = model(features).item()
+    else:
+      prediction = model.predict(features)[0]
+
+    return jsonify({"prediction": prediction})
+
+  except KeyError as e:
+    return jsonify({"error": f"Invalid request: {str(e.args[0])}"}), 400
+  except (ValueError, TypeError):
+    return jsonify({"error": "Invalid request: Please provide valid numeric values for all features."}), 400
+  except Exception as e:
+    return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 if __name__ == "__main__":
   app.run(debug=True, port=5000)
-
